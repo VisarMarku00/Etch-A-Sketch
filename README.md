6c84d4294b961873c82f4ac712096c9a4bad708b
Visar <96630229+VisarMarku00@users.noreply.github.com>
2023-09-06 22:48:56 +0200

    Create initial files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-A-Sketch
+
+This is a website made with HTMl, CSS and vanilla JavaScript.
---
+++ script.js
@@ -0,0 +1,7 @@
+//add global variables for some values
+
+//make a function to select the grid size of the function
+
+//Make a function to create a sketchpad
+
+//make a function to clear the sketchpad 
