a0e5773166200c64c8610f40b755785e27dc551e
Visar <96630229+VisarMarku00@users.noreply.github.com>
2023-09-08 21:33:27 +0200

    Add code to generate grid

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 //add global variables for some values
-
+let sketchPad = document.getElementById("sketch-pad");
+let gridSize;
+createGrid();
 //make a function to select the grid size of the function
+let gridSlider = document.querySelector("#grid-size-selector");
+gridSlider.addEventListener("input", (event) => {
+  cleanSketchPad();
+  gridSize = event.target.value;
+  createGrid(gridSize);
+});
 
 //Make a function to create a sketchpad
+function createGrid(gridSize) {
+  if (!gridSize) gridSize = 16;
+  for (let num = 0; num < gridSize * gridSize; num++) {
+    let pixel = document.createElement("div");
+    pixel.setAttribute("class", "pixel");
+    sketchPad.appendChild(pixel);
+  }
+  //console.log(gridSize);
+}
 
-//make a function to clear the sketchpad 
+//make a function to clear the sketchpad
+function cleanSketchPad() {
+  sketchPad.removeChild(sketchPad.lastChild);
+}
