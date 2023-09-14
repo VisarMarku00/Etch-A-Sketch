4f2484a58ed036427ea109f0824975974e413f98
Visar <96630229+VisarMarku00@users.noreply.github.com>
2023-09-14 20:52:34 +0200

    Finish sketch pad design

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //add global variables for some values
 let sketchPad = document.getElementById("sketch-pad");
 let gridSize;
+let brushColor;
+let padBackGroundColor;
 createGrid();
 
 //make a function to select the grid size of the function
@@ -14,14 +16,21 @@ gridSlider.addEventListener("input", (event) => {
 //Make a function to create a sketchpad
 function createGrid(gridSize) {
   if (!gridSize) gridSize = 16;
+  const widthPercentage = 100 / gridSize;
+  const heightPercentage = 100 / gridSize;
   for (let num = 0; num < gridSize * gridSize; num++) {
     let pixel = document.createElement("div");
     pixel.setAttribute("class", "pixel");
+    pixel.setAttribute(
+      "style",
+      `width:${widthPercentage}%; 
+      length:${heightPercentage}%;`
+    );
     sketchPad.appendChild(pixel);
   }
 }
 
 //make a function to clear the sketchpad
 function cleanSketchPad() {
-  sketchPad.removeChild(sketchPad.lastChild);
+  sketchPad.replaceChildren();
 }
