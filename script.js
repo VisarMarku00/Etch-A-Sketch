7e69c56c0048ffa3979a43b6d413dea06262f55d
Visar <96630229+VisarMarku00@users.noreply.github.com>
2023-09-15 13:23:16 +0200

    Finish primary brush functionality

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 //add global variables for some values
 let sketchPad = document.getElementById("sketch-pad");
 let gridSize;
-let brushColor;
-let padBackGroundColor;
 createGrid();
 
 //make a function to select the grid size of the function
 let selectorValue = document.querySelector("#value");
 let gridSlider = document.querySelector("#grid-size-selector");
 value.textContent = `${gridSlider.value} x ${gridSlider.value}`;
-gridSlider.addEventListener("input", (event) => {
+gridSlider.addEventListener("input", function allGridComponents(event) {
   cleanSketchPad();
   gridSize = event.target.value;
   value.textContent = `${gridSize} x ${gridSize}`;
@@ -27,13 +25,48 @@ function createGrid(gridSize) {
     pixel.setAttribute(
       "style",
       `width:${widthPercentage}%; 
-      length:${heightPercentage}%;`
+      height:${heightPercentage}%;`
     );
     sketchPad.appendChild(pixel);
   }
+  //make a function to change pixel color on hover
+  let pixels = document.querySelectorAll(".pixel");
+  pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseover", () => {
+      pixel.style.backgroundColor = brushColor;
+    });
+  });
 }
 
-//make a function to clear the sketchpad
+//make a function to clean previous grid size
 function cleanSketchPad() {
   sketchPad.replaceChildren();
 }
+
+//make function to select brushColor
+let brushColorSelector = document.querySelector("#brush-color");
+let brushColor = brushColorSelector.value;
+brushColorSelector.addEventListener("input", (event) => {
+  brushColor = event.target.value;
+});
+
+
+//make function to select background color and change it on pad
+// let padBackgroundColorSelector = document.querySelector("#background-color");
+// let padBackgroundColor = padBackgroundColorSelector.value;
+// padBackgroundColorSelector.addEventListener("input", (event) => {
+//   padBackgroundColor = event.target.value;
+//   pixels.forEach((pixel) =>{
+//     if(pixel.style.backgroundColor == padBackgroundColor){
+//       pixel.style.backgroundColor = brushColor;
+//     }
+//   })
+// });
+
+//function comparePixelColors(backgroundColor) {}
+
+//make a function for random colors on brush
+
+//make a function for shading pixel
+
+//make a function to lighten the pixel
